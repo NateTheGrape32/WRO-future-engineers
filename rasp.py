1f5949bb1dbece32d7d0686ad6e4801c8de890ee
AndrewDing-j <1adskht.new@gmail.com>
2026-07-12 12:06:09 -0700

    Included function to detect orange and blue lines on the track in order to assist with counting turns.

--- rasp.py
+++ rasp.py
@@ -9,29 +9,40 @@ SERIAL_PORT = "/dev/ttyACM0"
 BAUD = 115200
 
 SERVO_CENTER = 80
-SERVO_LEFT = 50
-SERVO_RIGHT = 110
-SERVO_RANGE = 105
+SERVO_LEFT = 65
+SERVO_RIGHT = 95
+SERVO_RANGE = 85
 
 MOTOR_STOP = 1500
 MOTOR_DRIVE = 1600
 
-ROI_LEFT = [0, 265, 240, 385]
-ROI_RIGHT = [400, 265, 640, 385]
+ROI_LEFT = [50, 295, 200, 640]
+ROI_RIGHT = [490, 295, 640, 640]
+ROI_CENTER = [200, 375, 490, 415]
 
 LAB_LOWER = np.array([0, 0, 0], dtype=np.uint8)
-LAB_UPPER = np.array([70, 255, 255], dtype=np.uint8)
+LAB_UPPER = np.array([80, 255, 255], dtype=np.uint8)
 
-ENTER_TURN_THRESH = 4000
-EXIT_TURN_THRESH = 8800
+ENTER_TURN_THRESH = 400
+EXIT_TURN_THRESH = 4000
 
 CONFIRM_FRAMES = 5
-EXIT_TIME_THRESH = 2.5
+EXIT_TIME_THRESH = 0.75
+
+BLUE_LOWER = np.array([95,80,80])
+BLUE_UPPER = np.array([130,255,255])
+
+ORANGE_LOWER = np.array([5,100,100])
+ORANGE_UPPER = np.array([25,255,255])
+
+LINE_THRESH = 1000
+STOP_DELAY = 5.0
 
 picam2 = Picamera2()
 picam2.preview_configuration.main.size = (640, 480)
 picam2.preview_configuration.main.format = "RGB888"
 picam2.preview_configuration.controls.FrameRate = 30
+
 picam2.preview_configuration.align()
 picam2.configure("preview")
 picam2.start()
@@ -49,6 +60,12 @@ confirmCount = 0
 turnEnterTime = None
 turnsCompleted = 0
 
+lineCount = 0
+linePresent = False
+
+stopScheduled = False
+stopTime = None
+
 roiArea = (ROI_LEFT[2] - ROI_LEFT[0]) * (ROI_LEFT[3] - ROI_LEFT[1])
 
 def clamp(value, low=25, high=130):
@@ -60,7 +77,6 @@ def send_servo(angle):
 
 def send_motor(speed):
     ser.write(f"$M{int(speed)}\n".encode())
-    print(speed)
 
 def draw_roi(img, roi, label = ""):
     x1, y1, x2, y2 = roi
@@ -86,7 +102,7 @@ def find_wall_area(frame, roi):
     roiImg = frame[y1:y2, x1:x2]
 
     lab = cv2.cvtColor(roiImg, cv2.COLOR_RGB2Lab)
-    lab = cv2.GaussianBlur(lab, (9, 9), 0)
+    lab = cv2.GaussianBlur(lab, (7, 7), 0)
 
     mask = cv2.inRange(lab, LAB_LOWER, LAB_UPPER)
 
@@ -116,8 +132,51 @@ def find_wall_area(frame, roi):
 
     return largest, mask, largestArea
 
+def detect_colored_line(frame):
 
-def pd_controller(error, kp=0.2, kd=0.1):
+    x1, y1, x2, y2 = ROI_CENTER
+
+    roi = frame[y1:y2, x1:x2]
+
+    hsv = cv2.cvtColor(roi, cv2.COLOR_RGB2HSV)
+
+    blueMask = cv2.inRange(
+        hsv,
+        BLUE_LOWER,
+        BLUE_UPPER
+    )
+
+    orangeMask = cv2.inRange(
+        hsv,
+        ORANGE_LOWER,
+        ORANGE_UPPER
+    )
+
+
+    mask = cv2.bitwise_or(
+        blueMask,
+        orangeMask
+    )
+
+
+    kernel = np.ones((3,3), np.uint8)
+
+    mask = cv2.morphologyEx(
+        mask,
+        cv2.MORPH_OPEN,
+        kernel
+    )
+
+
+    area = cv2.countNonZero(mask)
+
+
+    detected = area > LINE_THRESH
+
+
+    return detected, mask
+
+def pd_controller(error, kp=0.5, kd=0.1):
     global prevError
 
     derivative = error - prevError
@@ -128,7 +187,6 @@ def pd_controller(error, kp=0.2, kd=0.1):
     steering = SERVO_CENTER + output
     steering = clamp(steering)
     
-    print(steering)
 
     return steering
 
@@ -146,6 +204,42 @@ while True:
 
     leftContour, leftMask, leftArea = find_wall_area(frame, ROI_LEFT)
     rightContour, rightMask, rightArea = find_wall_area(frame, ROI_RIGHT)
+    
+    lineDetected, centerMask = detect_colored_line(frame)
+
+
+    if lineDetected and not linePresent:
+
+        lineCount += 1
+
+        print(f"Lines detected: {lineCount}")
+
+        linePresent = True
+
+
+        if lineCount == 23:
+
+            stopScheduled = True
+            stopTime = now
+
+            print("23rd line detected")
+            print("Stopping in 3 seconds")
+
+
+    elif not lineDetected:
+
+        linePresent = False
+        
+    if stopScheduled:
+
+        if now - stopTime >= STOP_DELAY:
+
+            send_motor(MOTOR_STOP)
+            send_servo(SERVO_CENTER)
+
+            print("Robot stopped")
+
+            break
 
     if mode == "FOLLOW_WALL":
 
@@ -202,6 +296,7 @@ while True:
 
     draw_roi(frame, ROI_LEFT, "LEFT")
     draw_roi(frame, ROI_RIGHT, "RIGHT")
+    draw_roi(frame, ROI_CENTER, "CENTER")
 
     if leftContour is not None:
         cv2.drawContours(frame, [leftContour], -1, (0, 255, 255), 2)
@@ -230,7 +325,7 @@ while True:
     )
 
     cv2.imshow("Frame", frame)
-
+    cv2.imshow("Center Mask", centerMask)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
